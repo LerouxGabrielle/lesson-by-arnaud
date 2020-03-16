7eb3bf1ddda05cb649a7320dc4d84ee377d4aefd
LerouxGabrielle <gabrielle22.leroux@laposte.net>
2020-03-16 13:27:35 +0100

    pourquoiçamarchepas

--- memory/src/App.js
+++ memory/src/App.js
@@ -9,24 +9,25 @@ class App extends React.Component {
   
     render() {
       const won = new Date().getSeconds() % 2 === 0
+
       return (
-        …
+        <div className="memory">
+          <GuessCount guesses={0} />
+          <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+          <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+          <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+          <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+          <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+          <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+  
+  
+        </div>
+      
         {won && <p>GAGNÉ !</p>}
       )
-    }
-    return (
-      <div className="memory">
-        <GuessCount guesses={0} />
-        <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
-        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
-        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
-        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
-        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
-        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
 
-
-      </div>
-    )
+    
+      }
   }
 }
 
