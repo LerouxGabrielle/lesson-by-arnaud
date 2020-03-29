b36ca16bb918e84fbaac1ea8793da595a2eefe98
LerouxGabrielle <gabrielle22.leroux@laposte.net>
2020-03-29 16:25:52 +0200

    pendu erreur react

---
+++ memory/src/composant/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = (
+<h1> jeu du pendu </h1>
+)
+
+
+export default Header
---
+++ memory/src/composant/interface.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './interface.css'
+
+const lettres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+const Bouton =  ({ bouton, feedback, onClick }) => (
+
+
+)
+
+
+export default interface
